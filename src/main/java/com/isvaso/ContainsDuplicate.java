90ce2b43caa122c5c437c4ed809b6905f3feb0f8
Avakian Ovsep <avakyan.hovsep@yandex.ru>
2023-04-06 00:11:43 +0300

    Remove period sign
    aklshdflakshdvflsavfasdlkfbasld

--- src/main/java/com/isvaso/ContainsDuplicate.java
+++ src/main/java/com/isvaso/ContainsDuplicate.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 /**
  * Given an integer array nums, return true if any value appears
- * at least twice in the array, and return false if every element is distinct.
+ * at least twice in the array, and return false if every element is distinct
  * */
 public class ContainsDuplicate {
 
